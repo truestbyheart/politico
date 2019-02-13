7d5ffb572053d0d218a7077b9151d676e6302bad
unknown <margalight@outlook.com>
2019-02-13 16:09:22 +0200

    [bug #163946395] fixing a pull request misteke from develop to gh-pages

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = process.env.PORT || 3200;
-app.use(bodyParser.json());
-
-module.exports = { app, PORT, bodyParser };
