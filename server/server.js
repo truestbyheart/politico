82db96867906e4fae3e215e8a0b5a600ded2e207
unknown <margalight@outlook.com>
2019-02-20 14:27:21 +0200

    [feature #164088060] added login route and introduces jsonwebtoken

--- server/server.js
+++ server/server.js
@@ -3,14 +3,16 @@ import bodyParser from 'body-parser';
 import partiesRouter from './route/parties';
 import officesRouter from './route/offices';
 import defaultRoute from './controller/default';
+import login from './route/login';
 
 
 export const app = express();
 export const PORT = process.env.PORT || 3200;
 app.use(bodyParser.json());
 
-app.use('/v1/parties/', partiesRouter);
-app.use('/v1/offices/', officesRouter);
+app.use('/api/v1/parties/', partiesRouter);
+app.use('/api/v1/offices/', officesRouter);
+app.use('/api/v1/login/', login);
 app.use('/', defaultRoute);
 
 
