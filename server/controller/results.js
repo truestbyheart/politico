c378885bca1b10fb6406b5337fea0905729d3ca7
unknown <margalight@outlook.com>
2019-02-21 12:51:13 +0200

    [feature #164134961] Created the result route and get back the results

--- server/controller/results.js
+++ server/controller/results.js
@@ -5,10 +5,17 @@ const result = (req, res) => {
   const query = 'SELECT candidate,office,CAST(COUNT(*) AS Int) AS result FROM  vote WHERE office=$1 GROUP BY candidate,office';
   pool.query(query, [id])
     .then((results) => {
-      res.status(200).json({
-        status: 200,
-        data: results.rows,
-      });
+      if (results.rowCount > 0) {
+        res.status(200).json({
+          status: 200,
+          data: results.rows,
+        });
+      } else {
+        res.status(200).json({
+          status: 200,
+          message: 'There no results to display at the moment',
+        });
+      }
     });
 };
 
